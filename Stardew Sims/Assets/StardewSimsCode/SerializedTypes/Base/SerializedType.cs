294c1192764d01947878f4aaa3d6f4090411139f
Vitor Barcellos <vitor@doubledashstudios.com>
2023-08-20 16:13:50 -0300

    Added serialized types code

---
+++ Stardew Sims/Assets/StardewSimsCode/SerializedTypes/Base/SerializedType.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace StardewSimsCode.SerializedTypes.Base
+{
+    public abstract class SerializedType<T> : ScriptableObject
+    {
+        [SerializeField] protected T _initialValue;
+        [SerializeField] protected T _runtimeValue;
+        
+        public T Value => _runtimeValue;
+
+        public void OnEnable()
+        {
+            _runtimeValue = _initialValue;
+        }
+
+        public void SetValue(T value)
+        {
+            _runtimeValue = value;
+        }
+    }
+}
---
+++ Stardew Sims/Assets/StardewSimsCode/SerializedTypes/Implementations/SerializedInt.cs
@@ -0,0 +1,11 @@
+﻿using StardewSimsCode.SerializedTypes.Base;
+using UnityEngine;
+
+namespace StardewSimsCode.SerializedTypes.Implementations
+{
+    [CreateAssetMenu(fileName = "NewSerializedInt", menuName = "StardewSims/SerializedTypes/Int")]
+    public class SerializedInt : SerializedType<int>
+    {
+        
+    }
+}
